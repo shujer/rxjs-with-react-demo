4a25d359edcc20dbf90b781c13d95120f42ad07b
shujer <zhangshujin@xiao100.com>
2021-02-09 10:56:55 +0800

    chore: addon console

---
+++ .storybook/manager.js
@@ -0,0 +1,15 @@
+import { addons } from "@storybook/addons";
+
+addons.setConfig({
+  isFullscreen: false,
+  showNav: true,
+  showPanel: true,
+  panelPosition: "right",
+  sidebarAnimations: true,
+  enableShortcuts: true,
+  isToolshown: true,
+  theme: undefined,
+  selectedPanel: "Actions",
+  initialActive: "sidebar",
+  showRoots: false,
+});
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
+import { withConsole } from "@storybook/addon-console";
+
+const optionsCallback = (options) => ({
+  panelExclude: [...options.panelExclude, /Warning|warn/],
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+export const decorators = [
+  (storyFn, context) => withConsole(optionsCallback)(storyFn)(context),
+];
